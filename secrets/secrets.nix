9b0edccced6cd49190ec6b20bb73f4fea1144f5c
Brett Milford <brettmilford@gmail.com>
2023-07-06 21:58:46 +0800

    repo cleanup

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,6 @@ let
   thamrys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNT9L1KhAcQAxYOpXBy61dRr00sWVty4b/d/ZjXhGAg";
 in
 {
-  "privkey.age".publicKeys = [ thamrys brett ];
   "node_exporter.crt.age".publicKeys = [ thamrys brett ];
   "node_exporter.key.age".publicKeys = [ thamrys brett ];
   "node_exporter_config.age".publicKeys = [thamrys brett ];
