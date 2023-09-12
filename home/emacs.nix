29ad4c5e0ed145a0cacc9fb39b8231b5250a1cef
Brett Milford <brettmilford@gmail.com>
2023-09-13 09:28:49 +1000

    improving emacs config

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,23 +1,61 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [
-    nixfmt
-    ripgrep
+    binutils
+    git
+    (ripgrep.override { withPCRE2 = true; })
+    gnutls
     fd
+    imagemagick
+    pinentry_emacs
+    zstd
+    nixfmt
+    editorconfig-core-c
+    sqlite
     (aspellWithDicts (d: [d.en]))
+    emacs-all-the-icons-fonts
+  ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
     terminal-notifier
   ];
 
   programs.emacs = {
     enable = true;
-    #package = pkgs.emacsNativeComp;
+    package = ((pkgs.emacsPackagesFor pkgs.emacsNativeComp).emacsWithPackages
+        (epkgs: [ epkgs.vterm ]));
   };
 
-  home.file.doom-emacs = {
-    source = ../config/doom/.config/doom;
-    recursive = true;
-    target = ".config/doom";
-    #onChange = ".emacs.d/bin/doom sync";
+  home = {
+    sessionVariables = {
+      EMACSDIR = "${config.xdg.configHome}/emacs";
+      DOOMDIR = "${config.xdg.configHome}/doom";
+      DOOMLOCALDIR = "${config.xdg.configHome}/doom-local";
+    };
+    sessionPath = [ "${config.xdg.configHome}/emacs/bin"];
+  };
+
+  xdg = {
+    enable = true;
+    configFile = {
+      "doom" = {
+        source = ../config/doom/.config/doom;
+        recursive = true;
+        onChange = "${pkgs.writeShellScript "doom-change" ''
+          export PATH="$PATH:$HOME/.nix-profile/bin"
+          export DOOMDIR="${config.home.sessionVariables.DOOMDIR}"
+          export DOOMLOCALDIR="${config.home.sessionVariables.DOOMLOCALDIR}"
+          export EMACSDIR="${config.home.sessionVariables.EMACSDIR}"
+          if [ ! -d "EMACSDIR" ]; then
+            git clone --depth 1 https://github.com/doomemacs/doomemacs.git $EMACSDIR
+          fi
+          if [ ! -d "$DOOMLOCALDIR" ]; then
+            ${config.xdg.configHome}/emacs/bin/doom -f install
+          else
+            ${config.xdg.configHome}/emacs/bin/doom sync -u
+          fi
+        ''}";
+
+      };
+    };
   };
 
   ## TODO: how to use nix-darwin's services.emacs here?
