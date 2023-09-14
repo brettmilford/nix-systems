14d11407f5c7d200adf807f563ace9a75f3858b9
Brett Milford <brettmilford@gmail.com>
2023-09-14 11:15:17 +1000

    updated keys

--- hosts/nixos/cloud.nix
+++ hosts/nixos/cloud.nix
@@ -7,6 +7,7 @@
     extraGroups = ["wheel" "networkmaanger"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlB/hd55JJCoIb8EDBvvwfrdGtTOli5H+d+3o0wqxYR brett@thamrys"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB9iwf2c7cAHQQpfkImGNDeZnYPzGbudZcZaBWkS03mu bmj"
     ];
   };
 
