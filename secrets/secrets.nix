f26f24f0d6b7382931b3565b8416e51d3b70ce80
Brett Milford <brettmilford@gmail.com>
2023-11-09 09:54:38 +1000

    updates

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,10 +1,11 @@
 let
   brett = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlB/hd55JJCoIb8EDBvvwfrdGtTOli5H+d+3o0wqxYR";
   thamrys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNT9L1KhAcQAxYOpXBy61dRr00sWVty4b/d/ZjXhGAg";
+  Calliope = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgKDS5T7rCHlU57ALKWHS6DSlPIcCBbbjfUlrUUBHNk";
 in
 {
   "node_exporter.crt.age".publicKeys = [ thamrys brett ];
   "node_exporter.key.age".publicKeys = [ thamrys brett ];
   "node_exporter_config.age".publicKeys = [thamrys brett ];
-  "nextcloud.age".publicKeys = [thamrys brett ];
+  "nextcloud.age".publicKeys = [thamrys Calliope brett ];
 }
