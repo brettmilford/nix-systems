a61fe730e8f1521f20cccd7f12c49223eb1135c2
Brett Milford <brettmilford@gmail.com>
2023-11-09 14:34:15 +1000

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   brett = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAlB/hd55JJCoIb8EDBvvwfrdGtTOli5H+d+3o0wqxYR";
   thamrys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNT9L1KhAcQAxYOpXBy61dRr00sWVty4b/d/ZjXhGAg";
-  Calliope = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINgKDS5T7rCHlU57ALKWHS6DSlPIcCBbbjfUlrUUBHNk";
+  Calliope = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICYnaqrEbdb9apQNMccZgfM0YRX6VTi1JPS+BmVd8Vb0";
 in
 {
   "node_exporter.crt.age".publicKeys = [ thamrys brett ];
