47441b46c297da78cb6bbb03f1a239834017d3ac
Brett Milford <brettmilford@gmail.com>
2025-06-17 23:23:13 +1000

    Eurydice: upgrade via 24.11
    - prep display manager change
    - deployments modules

--- hosts/nixos/desktop.nix
+++ hosts/nixos/desktop.nix
@@ -5,6 +5,11 @@
   options,
   ...
 }: {
+
+  imports = [
+    ./gnome-desktop.nix
+  ];
+
   networking.networkmanager.enable = true;
 
   i18n.extraLocaleSettings = {
@@ -22,52 +27,9 @@
 
   services.xserver = {
     enable = true;
-    layout = "au";
-  };
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome = {
-    enable = true;
-    favoriteAppsOverride = ''
-      [org.gnome.shell]
-      favorite-apps=[ 'firefox.desktop', 'org.gnome.Console.desktop', 'org.gnome.Nautilus.desktop' ]
-    '';
+    xkb.layout = "au";
   };
 
-  environment.gnome.excludePackages =
-    (with pkgs; [
-      gnome-photos
-      gnome-tour
-      snapshot
-      gedit # text editor
-    ])
-    ++ (with pkgs.gnome; [
-      cheese # webcam tool
-      gnome-music
-      gnome-terminal
-      epiphany # web browser
-      geary # email reader
-      #evince # document viewer
-      gnome-characters
-      totem # video player
-      tali # poker game
-      iagno # go game
-      hitori # sudoku game
-      atomix # puzzle game
-      yelp # Help view
-      gnome-contacts
-      gnome-initial-setup
-      gnome-maps
-    ]);
-
-  environment.systemPackages = with pkgs; [
-    firefox
-    bitwarden
-    gnomeExtensions.appindicator
-  ];
-
-  services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
-
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -92,7 +54,7 @@
 
   services.avahi = {
     enable = true;
-    nssmdns = true;
+    nssmdns4 = true;
     openFirewall = true;
   };
 }
--- hosts/nixos/eurydice/default.nix
+++ hosts/nixos/eurydice/default.nix
@@ -8,51 +8,36 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./opnsense.nix
     ../common.nix
     ../cloud.nix
     ../desktop.nix
     ../zerotierone.nix
-    ../virt.nix
     ../../../deployments
   ];
 
   boot.loader.efi.canTouchEfiVariables = false;
 
   boot.loader.grub = {
-	enable = true;
-	zfsSupport = true;
-	efiSupport = true;
-	efiInstallAsRemovable = true;
-	mirroredBoots = [
-		{ devices = ["nodev"]; path = "/boot";}
-	];
+    enable = true;
+    zfsSupport = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    mirroredBoots = [
+      { devices = ["nodev"]; path = "/boot";}
+    ];
   };
 
   networking.hostName = "eurydice";
   networking.hostId = "04ca88ad";
   networking.firewall.enable = true;
   networking.firewall.allowPing = true;
-  services.xserver.displayManager.gdm.autoSuspend = false;
   environment.systemPackages = with pkgs; [
     iw
   ];
 
-  # common reverse proxy
-  security.acme = {
-	  acceptTerms = true;
-	  defaults.email = "admin+acme@example.org";
-  };
-  networking.firewall.allowedTCPPorts = [80 443];
-
-  # Enable container-based service deployments
   services.deployments = {
-    enable = true;
-    
-    # Enable specific services
-    elasticsearch.enable = true;
     homeAssistant.enable = true;
     unifi.enable = true;
   };
-
-
 }
