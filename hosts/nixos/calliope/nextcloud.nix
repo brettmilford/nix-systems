bf5cc0ec2de5e1723f276b20338930efdf0c4b38
Brett Milford <brettmilford@gmail.com>
2025-06-18 17:06:13 +1000

    Update Nextcloud from version 30 to 31

--- hosts/nixos/calliope/nextcloud.nix
+++ hosts/nixos/calliope/nextcloud.nix
@@ -12,7 +12,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = "nextcloud.cirriform.au";
     config = {
       dbtype = "pgsql";
@@ -22,7 +22,7 @@
     appstoreEnable = true;
     extraApps = {
       inherit
-        (pkgs.nextcloud30Packages.apps)
+        (pkgs.nextcloud31Packages.apps)
         calendar
         contacts
         ;
