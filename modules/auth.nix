10ebf03ec2212de2cab567545ca9d7109e9d331e
Brett Milford <brettmilford@gmail.com>
2025-07-17 16:13:50 +1000

    harden auth config

--- modules/auth.nix
+++ modules/auth.nix
@@ -41,6 +41,9 @@ in
         http-enabled = true;
         http-port = 8080;
         proxy-headers = "xforwarded";
+        spi-login-protocol-openid-connect-legacy-logout-redirect-uri = "false";
+        log-level = "INFO";
+        log-console-format = "%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c] (%t) %s%e%n";
         http-max-queued-requests = "1000";
         Xms = "1g";
         Xmx = "2g";
@@ -78,6 +81,47 @@ in
           '';
         };
       };
+    };
+    services.fail2ban = {
+      enable = true;
+      jails = {
+        keycloak-auth = ''
+          enabled = true
+          backend = systemd
+          filter = keycloak-auth
+          maxretry = 5
+          findtime = 600
+          bantime = 3600
+          action = iptables-multiport[name=keycloak-auth, port="http,https"]
+        '';
+        keycloak-admin = ''
+          enabled = true
+          backend = systemd
+          filter = keycloak-admin
+          maxretry = 3
+          findtime = 300
+          bantime = 86400
+          action = iptables-multiport[name=keycloak-admin, port="http,https"]
+        '';
+      };
+    };
+
+    # Fail2ban filters for Keycloak
+    environment.etc = {
+      "fail2ban/filter.d/keycloak-auth.conf".text = ''
+        [Definition]
+        failregex = ^.*keycloak.*WARN.*type=LOGIN_ERROR.*user=.* error=invalid_user_credentials.*ip=<HOST>.*$
+                    ^.*keycloak.*WARN.*type=LOGIN_ERROR.*user=.* error=user_not_found.*ip=<HOST>.*$
+                    ^.*keycloak.*WARN.*type=LOGIN_ERROR.*user=.* error=user_disabled.*ip=<HOST>.*$
+        journalmatch = _SYSTEMD_UNIT=keycloak.service
+      '';
+
+      "fail2ban/filter.d/keycloak-admin.conf".text = ''
+        [Definition]
+        failregex = ^.*nginx.*client: <HOST>.*"(GET|POST).*/admin/.*HTTP.*" (40[1-4]|50[0-3]).*$
+        journalmatch = _SYSTEMD_UNIT=nginx.service
+      '';
+
     };
   };
 }
