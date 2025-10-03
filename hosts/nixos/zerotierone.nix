5441822075a242ad6a51856213b56c1fc90b38f5
Brett Milford <brettmilford@gmail.com>
2025-10-03 14:55:35 +1000

    Use DNS from router

--- hosts/nixos/zerotierone.nix
+++ hosts/nixos/zerotierone.nix
@@ -8,4 +8,9 @@
     enable = true;
     joinNetworks = ["ebe7fbd44553bc18"];
   };
+
+  networking = lib.mkIf config.services.zerotierone.enable {
+    nameservers = [ "172.22.0.1" ];
+    search = [ "cirriform.au" ];
+  };
 }
