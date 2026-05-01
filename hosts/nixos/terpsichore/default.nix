ff7d42f53f319141adb18461d72cab85007d221d
Brett Milford <brettmilford@gmail.com>
2026-05-01 11:48:41 +1000

    Re-encrypt secrets with terpsichore host key, update sasl path

--- hosts/nixos/terpsichore/default.nix
+++ hosts/nixos/terpsichore/default.nix
@@ -49,5 +49,5 @@ in
 
   services.postgresql.package = pkgs.postgresql_16;
 
-  services.mail-relay.secretPaths.postfix-sasl-passwd = "/var/lib/postfix/conf/sasl_passwd";
+  services.mail-relay.secretPaths.postfix-sasl-passwd = "/var/lib/postfix/sasl_passwd";
 }
