e4f1eb991029af3595a05f364bfffa6814a4d12e
Brett Milford <brettmilford@gmail.com>
2026-05-01 13:28:16 +1000

    Remove bomftp and pgvector from calliope

--- hosts/nixos/calliope/default.nix
+++ hosts/nixos/calliope/default.nix
@@ -5,7 +5,8 @@
   pkgs,
   modulesPath,
   ...
-}: {
+}:
+{
   imports = [
     ./hardware-configuration.nix
     (modulesPath + "/profiles/headless.nix")
@@ -27,11 +28,7 @@
 
   services.postgresql = {
     enable = true;
-    # Removing this will triger the install of a newer version of postgresql without migrating the data
     package = pkgs.postgresql_14;
-    extensions = with pkgs.postgresql_14.pkgs; [
-      pgvector
-    ];
   };
 
   services.kubo = {
@@ -52,10 +49,12 @@
         "/ip4/127.0.0.1/tcp/4001"
         "/ip4/127.0.0.1/udp/4001/quic-v1"
       ];
-      Peering.Peers = [{
-        "ID" = "12D3KooWT1Uq4VFBL8eLXumcqoMP3oYer4oYQSPbvjTg981FtiFB";
-        "Addrs" = ["/ip4/192.168.10.31/tcp/4001"];
-      }];
+      Peering.Peers = [
+        {
+          "ID" = "12D3KooWT1Uq4VFBL8eLXumcqoMP3oYer4oYQSPbvjTg981FtiFB";
+          "Addrs" = [ "/ip4/192.168.10.31/tcp/4001" ];
+        }
+      ];
       Addresses.NoAnnounce = [
         "/ip4/10.0.0.0/ipcidr/8"
         "/ip4/100.64.0.0/ipcidr/10"
@@ -75,22 +74,22 @@
         "/ip6/fe80::/ipcidr/10"
       ];
       Swarm.AddrFilters = [
-          "/ip4/10.0.0.0/ipcidr/8"
-          "/ip4/100.64.0.0/ipcidr/10"
-          "/ip4/169.254.0.0/ipcidr/16"
-          #"/ip4/172.16.0.0/ipcidr/12"
-          "/ip4/192.0.0.0/ipcidr/24"
-          "/ip4/192.0.2.0/ipcidr/24"
-          #"/ip4/192.168.0.0/ipcidr/16"
-          "/ip4/198.18.0.0/ipcidr/15"
-          "/ip4/198.51.100.0/ipcidr/24"
-          "/ip4/203.0.113.0/ipcidr/24"
-          "/ip4/240.0.0.0/ipcidr/4"
-          "/ip6/100::/ipcidr/64"
-          "/ip6/2001:2::/ipcidr/48"
-          "/ip6/2001:db8::/ipcidr/32"
-          "/ip6/fc00::/ipcidr/7"
-          "/ip6/fe80::/ipcidr/10"
+        "/ip4/10.0.0.0/ipcidr/8"
+        "/ip4/100.64.0.0/ipcidr/10"
+        "/ip4/169.254.0.0/ipcidr/16"
+        #"/ip4/172.16.0.0/ipcidr/12"
+        "/ip4/192.0.0.0/ipcidr/24"
+        "/ip4/192.0.2.0/ipcidr/24"
+        #"/ip4/192.168.0.0/ipcidr/16"
+        "/ip4/198.18.0.0/ipcidr/15"
+        "/ip4/198.51.100.0/ipcidr/24"
+        "/ip4/203.0.113.0/ipcidr/24"
+        "/ip4/240.0.0.0/ipcidr/4"
+        "/ip6/100::/ipcidr/64"
+        "/ip6/2001:2::/ipcidr/48"
+        "/ip6/2001:db8::/ipcidr/32"
+        "/ip6/fc00::/ipcidr/7"
+        "/ip6/fe80::/ipcidr/10"
       ];
       Swarm.RelayClient.Enabled = false;
       Swarm.EnableAutoRelay = false;
